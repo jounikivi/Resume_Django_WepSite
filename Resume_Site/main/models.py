3b90430c2b2cd8f43e140b7164228abd44cedf40
jounikivi <jounikivipera@gmail.com>
2022-07-25 15:17:52 +0300

    Update models.py

--- Resume_Site/main/models.py
+++ Resume_Site/main/models.py
@@ -142,6 +142,28 @@ class Blog(models. Model):
       self  .sluq = slugify(self.name)
     super(Blog, self).save(*args, **kwargs)
 
+  def __str__(self):
+      return self.name
+  
+  def get_absolute_url(self):
+    return f'/blog/{self.sluq}'
+
+class Certificate(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Certificates'
+        verbose_name = 'Certificate'
+
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+  
+
 
   
 
