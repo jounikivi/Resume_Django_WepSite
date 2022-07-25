525272ad370247aa381eb07a3e0f853bb1fe9f72
jounikivi <jounikivipera@gmail.com>
2022-07-25 15:06:58 +0300

    Update models.py

--- Resume_Site/main/models.py
+++ Resume_Site/main/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from distutils.command import upload
 import email
 from email import message
@@ -94,7 +95,33 @@ class Media(models.Model):
   def __str__(self):
       return self.name
   
+class Portfolio(models.Model):
 
+  class Meta:
+    verbose_name_plural = 'Portfolio Profiles'
+    verbose_name = 'Portfolio'
+    ordering =['name']
+
+  date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(max_length=200, blank=True, null=True)
+  description = models.CharField(max_length=500, blank=True, null=True)
+  body = models.CharField(blank=True, null=True)
+  image = models.ImageField(blank=True, null=True)
+  sluq = models.SlugField(null=True, blank=True)
+  is_active = models.BooleanField(default=True)
+
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self  .sluq = slugify(self.name)
+    super(Portfolio, self).save(*args, **kwargs)
+
+  def __str__(self):
+      return self.name
+
+  def get_absolute_url(self):
+    return f'/portfolio/{self.sluq}'
+  
+  
 
   
   
